8b9fa8008137bf64e6d8037b1fd7dee145092005
Motoya <yoshizne@outlook.jp>
2020-03-30 13:16:10 +0900

    Single click operation in tree view

--- source/View/FileTree.xaml.cs
+++ source/View/FileTree.xaml.cs
@@ -1,6 +1,8 @@
 ﻿
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 using System.Windows.Input;
 
 namespace Sidecab.View
@@ -15,19 +17,46 @@ namespace Sidecab.View
         }
 
         //======================================================================
-        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        private T FindParent<T>(DependencyObject child) where T : DependencyObject
         {
-            if (e.ClickCount == 2)
+            var parent = child;
+
+            //------------------------------------------------------------------
+            while ((parent != null) && (parent is TreeViewItem) == false)
             {
-                var directory = treeView_Directories.SelectedItem as Presenter.Directory;
-                if (directory != null) { directory.Open(); }
+                parent = VisualTreeHelper.GetParent(parent);
             }
+            //------------------------------------------------------------------
+
+            return parent as T;
         }
 
         //======================================================================
         private void button_Settings_Click(object sender, RoutedEventArgs e)
         {
             (App.Current.MainWindow as MainWindow)?.OpenSettingWindow();
+        }
+
+        //======================================================================
+        private void treeView_Directories_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is ToggleButton) return;
+
+            var clicked = FindParent<TreeViewItem>(e.OriginalSource as DependencyObject);
+            if (clicked != null) { clicked.IsExpanded = !clicked.IsExpanded; }
+        }
+
+        //======================================================================
+        private void treeView_Directories_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var clicked = FindParent<TreeViewItem>(e.OriginalSource as DependencyObject);
+            if (clicked != null)
+            {
+                e.Handled = true;
+
+                var directory = treeView_Directories.SelectedItem as Presenter.Directory;
+                if (directory != null) { directory.Open(); }
+            }
         }
     }
 }
