6519861fc030bb556b3656a87031509c7e2cb4b8
Yo-moya <yoshizne@outlook.jp>
2023-10-29 10:22:44 +0900

    Add a helper that provides screen information

---
+++ source/View/ScreenInfo.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace Sidecab.View
+{
+    public static class ScreenInfo
+    {
+        //----------------------------------------------------------------------
+        public static Rect GetWorkingArea(Window window)
+        {
+            var pixelsPerDip = VisualTreeHelper.GetDpi(window).PixelsPerDip;
+            var screen = Screen.FromPoint(new((int)window.Left, (int)window.Top));
+
+            return new(
+                screen.WorkingArea.Left   / pixelsPerDip,
+                screen.WorkingArea.Top    / pixelsPerDip,
+                screen.WorkingArea.Width  / pixelsPerDip,
+                screen.WorkingArea.Height / pixelsPerDip);
+        }
+    }
+}
