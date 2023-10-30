7430b14c69bbe6c01de808b3335487d65aa45262
Yo-moya <yoshizne@outlook.jp>
2023-10-30 23:37:38 +0900

    refactoring

--- source/View/SystemAttributes.cs
+++ source/View/SystemAttributes.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace Sidecab.View
 {
-    class SystemAttributes
+    static class SystemAttributes
     {
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         public static extern uint GetDoubleClickTime();
