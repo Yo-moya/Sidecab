b9a9c227dd773f034a2bb5438ab2333d403489da
Yo-moya <yoshizne@outlook.jp>
2023-11-04 23:49:12 +0900

    add property modifier

--- source/ObserverableObject.cs
+++ source/ObserverableObject.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.ComponentModel;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Sidecab
@@ -8,6 +9,32 @@ namespace Sidecab
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+
+        // Returns true if value-type property modified
+        //----------------------------------------------------------------------
+        protected bool ModifyProperty<T>(ref T target, T newValue,
+            [CallerMemberName] string propertyName = "") where T : struct
+        {
+            if (EqualityComparer<T>.Default.Equals(target, newValue)) return false;
+
+            target = newValue;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        //----------------------------------------------------------------------
+        protected bool ModifyNestedProperty<T>(
+            object propertyOwner, string propertyName, T newValue) where T : struct
+        {
+            var property = propertyOwner.GetType().GetProperty(propertyName);
+
+            if ((property.GetValue(propertyOwner) is not T oldValue)
+                || EqualityComparer<T>.Default.Equals(oldValue, newValue)) return false;
+
+            property.SetValue(propertyOwner, newValue);
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
 
         //----------------------------------------------------------------------
         protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
