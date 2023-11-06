39500b12ab4e5d74fee484a385fba43e751f7fa3
Yo-moya <yoshizne@outlook.jp>
2023-11-06 23:34:09 +0900

    refactoring

--- source/View/FolderTree.xaml.cs
+++ source/View/FolderTree.xaml.cs
@@ -12,13 +12,11 @@ namespace Sidecab.View
 {
     public partial class FolderTree : UserControl
     {
-        //----------------------------------------------------------------------
-        public Presenter.FolderTree Presenter
-        {
-            get { return this.DataContext as Presenter.FolderTree; }
-        }
+        private Presenter.FolderTree _presenter;
+        private readonly DispatcherTimer _clickTimer = new();
 
-        //======================================================================
+
+        //----------------------------------------------------------------------
         public FolderTree()
         {
             InitializeComponent();
@@ -29,58 +27,46 @@ namespace Sidecab.View
             // To avoid "Object reference not set to an instance of an object"
             if (isDesignTime == false)
             {
-                this.DataContext = new Presenter.FolderTree();
+                DataContext = _presenter = new();
 
                 var doubleClickTime = SystemAttributes.GetDoubleClickTime();
-                this.clickTimer.Interval = new TimeSpan(0, 0, 0, 0, (int)doubleClickTime);
-                this.clickTimer.Tick += clickTimer_Tick;
+                _clickTimer.Interval = new TimeSpan(0, 0, 0, 0, (int)doubleClickTime);
+                _clickTimer.Tick += clickTimer_Tick;
 
-                App.Core.Settings.PropertyChanged += this.OnSettingChanged;
+                App.Core.Settings.PropertyChanged += OnSettingChanged;
             }
         }
 
-        //======================================================================
-        ~FolderTree()
-        {
-            App.Core.Settings.PropertyChanged -= this.OnSettingChanged;
-        }
 
-
-        private DispatcherTimer clickTimer = new DispatcherTimer();
-
-
-        //======================================================================
+        //----------------------------------------------------------------------
         private void OnSettingChanged(object sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName.Contains("Font"))
             {
-                this.treeView_Directories.Items.Refresh();
-                this.treeView_Directories.UpdateLayout();
+                TreeView_Directories.Items.Refresh();
+                TreeView_Directories.UpdateLayout();
             }
         }
 
-        //======================================================================
+        //----------------------------------------------------------------------
         private T FindParent<T>(DependencyObject child) where T : DependencyObject
         {
             var parent = child;
-
-            //------------------------------------------------------------------
             while ((parent is object) && ((parent is TreeViewItem) == false))
             {
                 parent = VisualTreeHelper.GetParent(parent);
             }
-            //------------------------------------------------------------------
 
             return parent as T;
         }
 
-        //======================================================================
+        //----------------------------------------------------------------------
         private void clickTimer_Tick(object sender, EventArgs e)
         {
-            this.clickTimer.Stop();
+            _clickTimer.Stop();
         }
 
-        //======================================================================
+        //----------------------------------------------------------------------
         private void Button_AppMenu_Click(object sender, RoutedEventArgs e)
         {
             if (FindResource("ContextMenu_App") is ContextMenu menu)
@@ -90,60 +76,60 @@ namespace Sidecab.View
             }
         }
 
-        //======================================================================
+        //----------------------------------------------------------------------
         private void MenuItem_Settings_Click(object sender, RoutedEventArgs e)
         {
             (App.Current.MainWindow as TreeWindow)?.OpenSettingsWindow();
         }
 
-        //======================================================================
+        //----------------------------------------------------------------------
         private void MenuItem_CloseApp_Click(object sender, RoutedEventArgs e)
         {
             App.Current.Shutdown();
         }
 
-        //======================================================================
-        private void treeView_Directories_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        //----------------------------------------------------------------------
+        private void TreeView_Directories_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (FindParent<TreeViewItem>(e.OriginalSource as DependencyObject) is TreeViewItem)
             {
                 e.Handled = true;
 
-                var directory = this.treeView_Directories.SelectedItem as Presenter.Directory;
+                var directory = TreeView_Directories.SelectedItem as Presenter.Directory;
                 directory?.Open();
             }
         }
 
-        //======================================================================
-        private void manuItem_Pin_Click(object sender, RoutedEventArgs e)
+        //----------------------------------------------------------------------
+        private void MenuItem_Pin_Click(object sender, RoutedEventArgs e)
         {
-            if (this.treeView_Directories.SelectedItem is Presenter.Directory directory)
+            if (TreeView_Directories.SelectedItem is Presenter.Directory directory)
             {
-                this.Presenter.AddBookmark(directory);
+                _presenter.AddBookmark(directory);
             }
 
-            this.Presenter.SelectLastRoot();
+            _presenter.SelectLastRoot();
         }
 
-        //======================================================================
-        private void menuItem_OpenFolder_Click(object sender, RoutedEventArgs e)
+        //----------------------------------------------------------------------
+        private void MenuItem_OpenFolder_Click(object sender, RoutedEventArgs e)
         {
-            var directory = this.treeView_Directories.SelectedItem as Presenter.Directory;
+            var directory = TreeView_Directories.SelectedItem as Presenter.Directory;
             directory?.Open();
         }
 
-        //======================================================================
-        private void manuItem_CopyPath_Click(object sender, RoutedEventArgs e)
+        //----------------------------------------------------------------------
+        private void MenuItem_CopyPath_Click(object sender, RoutedEventArgs e)
         {
-            var directory = this.treeView_Directories.SelectedItem as Presenter.Directory;
+            var directory = TreeView_Directories.SelectedItem as Presenter.Directory;
             directory?.CopyPath();
         }
 
-        //======================================================================
-        private void treeView_Directories_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        //----------------------------------------------------------------------
+        private void TreeView_Directories_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             if (e.OriginalSource is ToggleButton) return;
-            if (this.clickTimer.IsEnabled) return;
+            if (_clickTimer.IsEnabled) return;
 
             if (FindParent<TreeViewItem>(e.OriginalSource as DependencyObject) is TreeViewItem clicked)
             {
@@ -153,13 +139,13 @@ namespace Sidecab.View
                     directory?.CollectSubdirectories();
                 }
 
-                clickTimer.Start();
+                _clickTimer.Start();
                 clicked.IsExpanded = !clicked.IsExpanded;
             }
         }
 
-        //======================================================================
-        private void treeView_Directories_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        //----------------------------------------------------------------------
+        private void TreeView_Directories_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
             var clicked = FindParent<TreeViewItem>(e.OriginalSource as DependencyObject);
             if (clicked is null)
@@ -170,14 +156,14 @@ namespace Sidecab.View
 
             clicked.IsSelected = true;
 
-            var directory = treeView_Directories.SelectedItem as Presenter.Directory;
-            this.manuItem_Pin.IsEnabled = directory?.HasSubdirectories ?? false;
+            var directory = TreeView_Directories.SelectedItem as Presenter.Directory;
+            MenuItem_Pin.IsEnabled = directory?.HasSubdirectories ?? false;
         }
 
-        //======================================================================
-        private void treeViewItem_Directories_Expanded(object sender, RoutedEventArgs e)
+        //----------------------------------------------------------------------
+        private void TreeViewItem_Directories_Expanded(object sender, RoutedEventArgs e)
         {
-            if (this.clickTimer.IsEnabled) return;
+            if (_clickTimer.IsEnabled) return;
 
             if (sender is TreeViewItem treeViewItem)
             {
@@ -186,7 +172,7 @@ namespace Sidecab.View
                     directory.CollectSubdirectories();
                 }
 
-                clickTimer.Start();
+                _clickTimer.Start();
             }
         }
     }
